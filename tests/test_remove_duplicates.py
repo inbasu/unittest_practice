520eb2bb030134a227955702e63e2d279ed0490e
inbasu <just.me.juncy@gmail.com>
2022-12-15 22:06:17 +0400

    remove_duplicates.py

---
+++ tests/test_remove_duplicates.py
@@ -0,0 +1,24 @@
+import problems.remove_duplicates as remove_duplicates
+from random import randint
+import unittest
+
+
+class TestlengthOfLastWord(unittest.TestCase):
+    def setUp(self):
+        self.remove_duplicates = remove_duplicates.Solution()
+
+    def test_random(self):
+        for l in range(1, 100):
+            test_list = sorted([randint(1, 10) for _ in range(l)])
+            self.remove_duplicates.nums = []
+            idx = self.remove_duplicates.removeDuplicates(test_list)
+            self.assertEqual(
+                self.remove_duplicates.nums[:idx], self.greed(test_list), msg=test_list
+            )
+
+    def greed(self, lst) -> list:
+        return sorted((list(set(lst))))
+
+
+if __name__ == "__main__":
+    unittest.main()
